295be9a3da0d2d91a8611a22a92da84133b42095
ulyweb <uly@ulynetwork.com>
2025-04-26 12:06:05 -0700

    Create Display-DistributionGroup.ps1

---
+++ scripts/Display-DistributionGroup.ps1
@@ -0,0 +1,4 @@
+Clear-host
+#To more info to be listed use -filter properties listing using command below:
+get-recipient "EmailAddressHERE!" -Filter * | fl *
+(get-distributiongroup "DLGroupNameHERE!").AcceptMessagesOnlyFrom  | Get-Recipient | Select DisplayName, Title, PrimarySmtpAddress, RecipientType
