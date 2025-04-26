2c14ea9dc9add2c55190f10a85d71f4e84f80d9b
ulyweb <uly@ulynetwork.com>
2025-04-26 12:34:41 -0700

    Update Display-DistributionGroup.ps1

--- scripts/Display-DistributionGroup.ps1
+++ scripts/Display-DistributionGroup.ps1
@@ -3,3 +3,4 @@ Clear-host
 get-recipient "EmailAddressHERE!" -Filter * | fl *
 #OR (get-recipient "EmailAddressHERE!" -Filter *) | fl *
 (get-distributiongroup "DLGroupNameHERE!").AcceptMessagesOnlyFrom  | Get-Recipient | Select Alias, PrimarySmtpAddress, Title
+(Get-RecipientPermission "Executive.Support").Trustee | Get-Recipient | Select Alias, PrimarySmtpAddress, Title
